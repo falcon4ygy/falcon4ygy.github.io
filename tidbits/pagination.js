07e10597481a75c52c62126b89b76b74864df4cf
absentmindless <39609598+absentmindless@users.noreply.github.com>
2019-11-23 23:31:19 -0500

    Create pagination.js

---
+++ tidbits/pagination.js
@@ -0,0 +1,10 @@
+var currentPage = 1;
+var numberPerPage = 2;
+var numberOfPages = 1;   // calculates the total number of pages
+
+var tidbits = [
+  "bi-jing-shen-ai-guo.html",
+  "lei-le-zou-le-san-le.html",
+  "zui-nuan-de-you-shang.html",
+  "na-shi-ni-li-kai-le-bei-jing-de-sheng-huo.html"
+];
