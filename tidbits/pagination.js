5daeb19e4ec26778a8919bc83db8c46adbb04e44
absentmindless <39609598+absentmindless@users.noreply.github.com>
2019-12-01 00:43:38 -0500

    Update pagination.js

--- tidbits/pagination.js
+++ tidbits/pagination.js
@@ -1,5 +1,5 @@
 var currentPage = 1;
-var numberPerPage = 2;
+var numberPerPage = document.getElementsByName("tidbit").length;
 var tidbits = [                     //this array contains all the tidbits; TODO store objects instead of strings for complex sorting
   "bi-jing-shen-ai-guo.html",
   "lei-le-zou-le-san-le.html",
@@ -14,12 +14,18 @@ function displayPage(number) {
   }
   else {
     //update button states
-      //find active one and remove color attribute
-      //find current one using getElementsByID()[number-1] and add class name
+      buttonUpdate(number);
     //includehtml of the ones that correspond to that page
     var startIndex = (currentPage - 1) * numberPerPage;
     var i;
     for (i = 0; i < numberPerPage; i++) {
-      //includehtml of (startIndex + i)
+      document.getElementsByName("tidbit")[i].setAttribute("w3-include-html", tidbits[startIndex + i]); 
   }
 }
+
+function buttonUpdate(number) {
+  //hide active
+  document.getElementsByClassName("w3-button w3-blue")[0].setAttribute("class", "w3-button"); 
+  //blue current
+  document.getElementsByName("pbutton")[number-1].setAttribute("class", "w3-button w3-blue"); 
+}  
