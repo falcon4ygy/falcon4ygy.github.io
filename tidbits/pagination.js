b8c70aaa5f0659e86c52f59ea49b686adfefa35c
absentmindless <39609598+absentmindless@users.noreply.github.com>
2019-12-01 09:52:22 -0500

    Update pagination.js

--- tidbits/pagination.js
+++ tidbits/pagination.js
@@ -1,5 +1,3 @@
-includeHTML();
-
 var currentPage = 1;
 var numberPerPage = document.getElementsByName("tidbit").length;
 var tidbits = [                     //this array contains all the tidbits; TODO store objects instead of strings for complex sorting
@@ -11,17 +9,21 @@ var tidbits = [                     //this array contains all the tidbits; TODO 
 var numberOfPages = Math.ceil(tidbits.length / numberPerPage);    //calculate number of pages
 
 function displayPage(number) {
-  if ((number < 1) || (number > numberOfPages)) { //do nothing }
+  if ((number < 1) || (number > numberOfPages)) { /* do nothing */  }
+  else if (number == currentPage) { /* also do nothing */ }
   else {
     //update button states
       buttonUpdate(number);
     //includehtml of the ones that correspond to that page
+    currentPage = number;
     var startIndex = (currentPage - 1) * numberPerPage;
     var i;
     for (i = 0; i < numberPerPage; i++) {
-      document.getElementsByName("tidbit")[i].setAttribute("w3-include-html", tidbits[startIndex + i]); 
+      var page = "/tidbits/"
+      document.getElementsByName("tidbit")[i].setAttribute("w3-include-html", page + tidbits[startIndex + i]); 
     }
   }
+  includeHTML();
 }
 
 function buttonUpdate(number) {
